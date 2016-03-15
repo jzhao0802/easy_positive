7650a406685ab35fa6345c09f9871f19c80f4d97
Lichao Wang kgxsapp101 <lichao.wang@uk.imshealth.com>
2016-03-15 19:13:33 +0000

    Squashed commit of the following:
    commit c8426e24f1d9bd7a847d160f6719cc81a9cd72b3
    Author: Lichao Wang kgxsapp101 <lichao.wang@uk.imshealth.com>
    Date:   Tue Mar 15 19:03:32 2016 +0000
    
        1. A bug in CalPrecisionAtRecall fixed in validifying the labels' values;
        2. The input labels are no longer converted to a factor variable for all
        models;
        3. Bugs are fixed in prediction using an ensemble;
        4. No longer saves the predictions for evaluation
    
    commit 6ba5400835914015fc445fc8847d810f71d4e57a
    Author: Lichao Wang kgxsapp101 <lichao.wang@uk.imshealth.com>
    Date:   Tue Mar 15 18:26:52 2016 +0000
    
        Bugs fixed in TrainWinnerLearners for referencing training y and X.
    
    commit c5362e2c2a0ca21afb4eabfc336abf7b1626ca90
    Author: Lichao Wang kgxsapp101 <lichao.wang@uk.imshealth.com>
    Date:   Tue Mar 15 18:19:37 2016 +0000
    
        A bug about returning predictions within cv from foreach is fixed.
    
    commit d4bcb292879dc6d3eb249c141811bc0ced689638
    Author: Lichao Wang kgxsapp101 <lichao.wang@uk.imshealth.com>
    Date:   Tue Mar 15 18:11:20 2016 +0000
    
        1. more unit test code for StratifyEasyDifficultPositives added (to check
        that the left out IDs in every fold after stratification combine to the
        original set of IDs);
        2. a bug in StratifyEasyDifficultPositives fixed.
    
    commit b3477f2351e0ac457ae81cfbc233bcde6c5d420a
    Author: Lichao Wang kgxsapp101 <lichao.wang@uk.imshealth.com>
    Date:   Tue Mar 15 16:16:38 2016 +0000
    
        A bug fixed for identifying weak learner types.
    
    commit 475e823b5b9bd8d2388341c3047927fc6b657f7c
    Author: Lichao Wang kgxsapp101 <lichao.wang@uk.imshealth.com>
    Date:   Tue Mar 15 16:13:32 2016 +0000
    
        A bug for reporting invalid weak learner type fixed.
    
    commit d78381ac57486c17bb08db293aa75b89f09b1986
    Author: Lichao Wang kgxsapp101 <lichao.wang@uk.imshealth.com>
    Date:   Tue Mar 15 16:11:24 2016 +0000
    
        A bug fixed for normalising the variables for evaluation according to the
        training + validation data.

--- scripts/run_ModelStack.R
+++ scripts/run_ModelStack.R
@@ -3,7 +3,7 @@ rm(list=ls())
 source("functions/ModelStack.R")
 
 main.arglist <- list()
-main.arglist$dataDir <- "../../../Results/2016-03-08/2016-03-08 15.57.03/"
+main.arglist$dataDir <- "../../../../Results/2016-03-08/2016-03-08 15.57.03/"
 
 main.arglist$bSelfEval <- T
 main.arglist$selfEval.kEvalFolds <- 2
@@ -11,28 +11,25 @@ main.arglist$kValiFolds <- 3
 
 main.arglist$posWeightMethod <- CON_POS_WEIGHT_METHOD$KNN
 main.arglist$similarityScoreFile <- 
-  "../../../Results/2016-03-09/2016-03-09 12.32.26/SimilarityScoreResult.csv"
+  "../../../../Results/2016-03-09/2016-03-09 12.32.26/SimilarityScoreResult.csv"
 
 # hyper-parameter grid
 
 main.arglist$bParallel <- F
-main.arglist$nCores2Use <- 15
 
 main.arglist$bBadPosWeighting <- T
 
-# main.arglist$weakLearnerSeed <- 
-#   list(LR_LASSO=list(logLambda=(-4:4)),
-#        SVM_LIN=list(logC=(-4:2)),
-#        SVM_RAD=list(logC=(-4:2), 
-#                     logGamma=seq(from=-4, to=1, by=1)),
-#        RF_BREIMAN=list(nTrees=c(50,100,200),
-#                        nVarsPerSplit=c(5, 10, 20),
-#                        nodeSize=c(1,2,4,8)),
-#        RF_CI=list(nTrees=c(50,100,200),
-#                   nVarsPerSplit=c(5, 10, 20)))
 main.arglist$weakLearnerSeed <- 
-  list(LR_LASSO=list(logLambda=(-1:1)))
-main.arglist$posNegRatios <- seq(from=0.8, to=1, by=0.2)
+  list(LR_LASSO=list(logLambda=(-4:4)),
+       SVM_LIN=list(logC=(-4:2)),
+       SVM_RAD=list(logC=(-4:2), 
+                    logGamma=seq(from=-4, to=1, by=1)),
+       RF_BREIMAN=list(nTrees=c(50,100,200),
+                       nVarsPerSplit=c(5, 10, 20),
+                       nodeSize=c(1,2,4,8)),
+       RF_CI=list(nTrees=c(50,100,200),
+                  nVarsPerSplit=c(5, 10, 20)))
+main.arglist$posNegRatios <- seq(from=0.2, to=1, by=0.2)
 
 main.arglist$targetRecall <- 0.05
 
